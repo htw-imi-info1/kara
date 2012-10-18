ff21aeae8e1103dcf6fa5851be54438d4718ac01
Dr Blinken <drblinken@gmail.com>
2012-10-18 15:09:52 +0200

    multimove - methods with parameters

---
+++ 02-00-multiMove/MyKara.java
@@ -0,0 +1,33 @@
+import greenfoot.*;
+
+/**
+ * MyKara is a subclass of Kara. Therefore, it inherits all methods of Kara: <p>
+ * 
+ * <i>MyKara ist eine Unterklasse von Kara. Sie erbt damit alle Methoden der Klasse Kara:</i> <p>
+ * 
+ * Actions:     move(), turnLeft(), turnRight(), putLeaf(), removeLeaf() <b>
+ * Sensors:     onLeaf(), treeFront(), treeLeft(), treeRight(), mushroomFront()
+ */
+public class MyKara extends Kara
+{
+    int counter = 0;
+ 
+    /**
+     * In the 'act()' method you can write your program for Kara <br>
+     * <i>In der Methode 'act()' koennen die Befehle fuer Kara programmiert werden</i>
+     */
+    public void act() 
+    {
+      multiMove(5);
+
+    } 
+
+    public void multiMove(int n){
+        int i = 0;
+        while (i < n){
+            move();
+            i++; // same as i = i +1;
+        }
+
+    }
+}
