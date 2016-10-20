ad9bc6f42cac80a869983c8aa94c30ba6cd04c25
B Kleinen <kleinen@infrastructure.de>
2016-10-20 15:41:07 +0200

    in class 20/10/2016 - walks but doesn't count correctly

--- 02-06-kara-in-a-box/MyKara.java
+++ 02-06-kara-in-a-box/MyKara.java
@@ -10,14 +10,35 @@ import greenfoot.*;
  */
 public class MyKara extends Kara
 {
+    int z = 0;
+    boolean ichLaufGradNachLinks = false;
+    public void zaehlen(){
+    z = z +1;}
 
+    public void drehen(){
+        if (ichLaufGradNachLinks) {
+            turnLeft();
+            move();
+            turnLeft();
+        } else{
+            turnRight();
+            move();
+            turnRight();
+        }	
+ichLaufGradNachLinks = !ichLaufGradNachLinks;
+    }
     /**
      * In the 'act()' method you can write your program for Kara <br>
      * <i>In der Methode 'act()' koennen die Befehle fuer Kara programmiert werden</i>
      */
     public void act() 
     {
-        
+        zaehlen();
+        if (treeFront()){
+            drehen();
+        } else {
+            move();
+        }  
     }
-   
+
 }
