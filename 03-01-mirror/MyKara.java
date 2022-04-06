ff9116aeef9a493416af600bd344292729dcef77
Barne Kleinen <barne.kleinen@htw-berlin.de>
2022-04-06 23:22:02 +0200

    mirror solution

--- 03-01-mirror/MyKara.java
+++ 03-01-mirror/MyKara.java
@@ -11,13 +11,52 @@ import greenfoot.*;
 public class MyKara extends Kara
 {
     /**
-     * In the 'act()' method you can write your program for Kara <br>
-     * <i>In der Methode 'act()' koennen die Befehle fuer Kara programmiert werden</i>
-     */
+     * pre-condition: Kara sits in the middle of the next line to mirror.
+     * post-condition: line is mirrored, 
+     *                 Kara sits in the middle of the next line to be mirrored
+    */
     public void act() 
     {
-        
+        mirrorLine();
+        if(treeFront()) 
+          Greenfoot.stop();
+        else
+          move();
     }  
-
+    /**
+     * pre-condition: Kara sits in the middle of the line to mirror.
+     * post-condition: Kara sits in the middle of the line to mirror,
+     *                 line is mirrored.
+     */
+    public void mirrorLine(){
+        int column = 1;
+        turnLeft();
+        while(mirrorLeaf(column++));
+        turnRight();
+    }
+    /**
+     * pre-cond.: kara sits in middle of line facing left
+     * post-cond: kara sits in middle of line facing left,
+     * 
+     */
+    public boolean mirrorLeaf(int column){
+        boolean foundTree = false;
+    
+        move(column);
+        foundTree = treeFront();
+        if(onLeaf()){
+            turnLeft();turnLeft();
+            move(column*2);
+            putLeaf();
+            turnRight();turnRight();
+            move(column);
+        }
+        else{
+            turnLeft();turnLeft();
+            move(column);
+            turnLeft();turnLeft();    
+        }
+        return !foundTree;
+    }
     
 }
