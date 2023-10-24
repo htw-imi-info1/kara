ce82e2d7408c448c980596e8a293fd5e7763dd37
Barne Kleinen <barne.kleinen@htw-berlin.de>
2023-10-24 13:50:20 +0200

    kara count leaves in clas 24.10.2023

--- 02-01-counting-leaves/MyKara.java
+++ 02-01-counting-leaves/MyKara.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import greenfoot.*;
 
 /**
@@ -24,3 +25,47 @@ public class MyKara extends Kara
 
     
 }
+=======
+import greenfoot.*;
+
+/**
+ * MyKara is a subclass of Kara. Therefore, it inherits all methods of Kara: <p>
+ * 
+ * <i>MyKara ist eine Unterklasse von Kara. Sie erbt damit alle Methoden der Klasse Kara:</i> <p>
+ * 
+ * Actions:     move(), turnLeft(), turnRight(), putLeaf(), removeLeaf() <b>
+ * Sensors:     onLeaf(), treeFront(), treeLeft(), treeRight(), mushroomFront()
+ */
+public class MyKara extends Kara
+{
+    int counter = 0;
+
+    /**
+     * In the 'act()' method you can write your program for Kara <br>
+     * <i>In der Methode 'act()' koennen die Befehle fuer Kara programmiert werden</i>
+     */
+    public void act() 
+    {
+
+        move();
+        if(onLeaf()){
+            counter = counter +1;
+        } 
+        if (treeFront()) 
+            Greenfoot.stop();
+
+    } 
+
+    public int countLeaves(){
+        int count = 0;
+        while(!treeFront()){
+
+            move();
+            if(onLeaf()){
+                count = count +1;
+            }
+        }
+        return count;
+    }
+}
+>>>>>>> Stashed changes
